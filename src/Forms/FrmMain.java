3e96e1f50686e2b443cb0415d2e5a0cb8c13b8ca
ilyasjan <ilyas5137@163.com>
2015-01-23 10:55:58 +0800

    Signed-off-by: ilyasjan <ilyas5137@163.com>

--- src/Forms/FrmMain.java
+++ src/Forms/FrmMain.java
@@ -8,18 +8,15 @@ import classs.ClipBoardController;
 import classs.OnButtonClick;
 
 @SuppressWarnings("serial")
-public class FrmMain extends JFrame implements Action
-{
+public class FrmMain extends JFrame implements Action {
 	/**
 	 * 
 	 */
-	public FrmMain()
-	{
+	public FrmMain() {
 		initComponents();
 	}
 
-	private void initComponents()
-	{
+	private void initComponents() {
 		jTextField1 = new JTextField();
 		jButton1 = new JButton();
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -31,33 +28,46 @@ public class FrmMain extends JFrame implements Action
 		jButton1.setFont(new java.awt.Font("UKIJ Tuz Tom", 0, 20)); // NOI18N
 		jButton1.setText("كۈچۈرۈش");
 		this.setResizable(false);
+
 		GroupLayout layout = new GroupLayout(getContentPane());
+
 		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING).addGroup(
-		        layout.createSequentialGroup().addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 326, GroupLayout.PREFERRED_SIZE)
-		                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED).addComponent(jButton1).addGap(15, 15, 15)));
-		layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING).addGroup(
-		        layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		                .addComponent(jButton1)));
+
+		layout.setHorizontalGroup(layout.createParallelGroup(
+				GroupLayout.Alignment.TRAILING)
+				.addGroup(
+						layout.createSequentialGroup()
+								.addContainerGap(GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE)
+								.addComponent(jTextField1,
+										GroupLayout.PREFERRED_SIZE, 600,
+										GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(
+										LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(jButton1).addGap(15, 15, 15)));
+		layout.setVerticalGroup(layout.createParallelGroup(
+				GroupLayout.Alignment.TRAILING).addGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(jTextField1, GroupLayout.PREFERRED_SIZE,
+								GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(jButton1)));
 		pack();
 	}
 
-	private JButton    jButton1;
+	private JButton jButton1;
 	private JTextField jTextField1;
 
-	public void ClearText()
-	{
+	public void ClearText() {
 		jTextField1.setText("");
 	}
 
-	public String getTextValue()
-	{
+	public String getTextValue() {
 		return jTextField1.getText();
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0)
-	{
+	public void actionPerformed(ActionEvent arg0) {
 		String string = getTextValue();
 		BidiBulider bulider = new BidiBulider();
 		String rev = bulider.Build(string);
@@ -67,20 +77,17 @@ public class FrmMain extends JFrame implements Action
 	}
 
 	@SuppressWarnings("deprecation")
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		FrmMain frmMain = new FrmMain();
 		frmMain.show();
 	}
 
 	@Override
-	public Object getValue(String key)
-	{
+	public Object getValue(String key) {
 		return null;
 	}
 
 	@Override
-	public void putValue(String key, Object value)
-	{
+	public void putValue(String key, Object value) {
 	}
 }
